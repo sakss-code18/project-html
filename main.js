cafa6f430e361683c97918dda901a4a9e44452bf
sakss-code18 <rathodsakshi1807@gmail.com>
2026-01-03 13:33:52 +0530

    Initial commit – portfolio-html

---
+++ main.js
@@ -0,0 +1,9 @@
+const toggle = document.getElementById("themeToggle");
+
+
+toggle.addEventListener("click", () => {
+document.body.classList.toggle("dark");
+toggle.textContent = document.body.classList.contains("dark")
+? "Light Mode"
+: "Dark Mode";
+});
